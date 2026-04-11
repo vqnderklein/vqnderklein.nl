e61a5bc0531194f3dc022c50beed1fd76abe4d70
Mathijs <92179923+ThijsKlein@users.noreply.github.com>
2026-04-11 11:05:31 +0200

    Deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,19 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+	plugins: [sveltekit()],
+	server: {
+	  host: '0.0.0.0',
+	  port: 3000,
+	  watch: {
+		usePolling: true
+	  },
+	  proxy: {
+		'/api': {
+		  target: 'http://backend:3000',
+		  changeOrigin: true
+		}
+	  }
+	}
+  });
+  
