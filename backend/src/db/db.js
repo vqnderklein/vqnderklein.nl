186bb044623eafba92fb1fbbe4a9eb83ae2d78f7
Mathijs <92179923+ThijsKlein@users.noreply.github.com>
2026-04-12 18:03:00 +0200

    db update?

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,6 +3,7 @@ import mysql from 'mysql2/promise';
 export const db = await mysql.createPool({
 	host: process.env.MYSQL_HOST,
 	user: process.env.MYSQL_USER,
+	port: 3306,
 	password: process.env.MYSQL_PASSWORD,
 	database: process.env.MYSQL_DATABASE
 });
